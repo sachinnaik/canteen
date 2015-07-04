bd4d0252c65bec9d7438d89302a172fb5c2b5fe7
satyapriya <subudhisatyapriya@gmail.com>
2015-07-04 17:29:23 +0530

    Add weekly-menu page then change report page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+	def is_active?(link_path)
+	  if current_page?(link_path)
+	    "active"
+	  else
+	    ""
+	  end
+ 	end
+ 	def is_active2?(link_path1, link_path2)
+	  if current_page?(link_path1)
+	    "active"
+	  elsif current_page?(link_path2)
+	  	"active"
+	  else
+	  	""
+	  end
+ 	end
 end
